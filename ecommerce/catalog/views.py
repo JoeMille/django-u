3c9361aa56d69f83b14a061019c44720c4965220
Joe Miller <98966875+JoeMille@users.noreply.github.com>
2024-03-20 13:41:34 +0000

    Created user registration view and installed further django reg and auth features.

--- ecommerce/catalog/views.py
+++ ecommerce/catalog/views.py
@@ -1,8 +1,10 @@
 import stripe
 from django.conf import settings
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 
 
@@ -10,3 +12,15 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'catalog/index.html')
+
+# User Registration View
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
